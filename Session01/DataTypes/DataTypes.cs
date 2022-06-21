6d65710fb20b335e76c02e44592816568e8c2128
Chutiendat26092003 <chutiendat148@gmail.com>
2022-06-21 10:42:29 +0700

    C-sharp

---
+++ Session01/DataTypes/DataTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Example
+{
+   static void Main(string[] args)
+    {
+        int intVal;
+        double dblVal;
+        string strVal;
+        intVal = 10;
+        dblVal = 3.142;
+        strVal = "Fpt Aptech";
+        Console.WriteLine("{0} is a integer value", intVal);
+        Console.WriteLine("{0} is a double value", dblVal);
+        Console.WriteLine("{0} is a an string", strVal);
+        Console.ReadLine();
+
+    }
+}
---
+++ Session01/ImplicitilyTypedLocal/ImplicitilyTypedLocal.cs
@@ -0,0 +1,15 @@
+﻿class ImplicitilyTypedLocal
+{
+    static void Main(string[] args)
+    {
+        var i = 5;
+        var s = "hello";
+        var d = 1.0;
+        Console.WriteLine("i*1: " +i * 1);
+
+        Console.WriteLine("s in upper case: " + s.ToUpper());
+
+        Console.WriteLine("type of d:" + d.GetType());
+        Console.ReadLine();
+    }
+}
