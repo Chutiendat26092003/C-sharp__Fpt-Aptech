a7a3af710512f21e75c3c3bc35404a3736000b7c
Chutiendat26092003 <chutiendat148@gmail.com>
2022-06-23 11:10:37 +0700

    C-sharp_ ss4

---
+++ session4/cs11/cs11.cs
@@ -0,0 +1,9 @@
+﻿int num;
+Console.WriteLine("Even Numbers");
+for (num = 0; num <= 11; num++)
+{
+    if ((num % 2 ) == 0)
+    {
+        Console.WriteLine(num);
+    }
+}
---
+++ session4/cs8/cs8.cs
@@ -0,0 +1,11 @@
+﻿int num = 1;
+Console.WriteLine("Event Numbers");
+while (num <= 11)
+{
+    if ((num % 2) == 0 )
+    {
+        Console.WriteLine(num);
+    }
+    num = num + 1;
+}
+
