492506c7e646b07307bfbfcefd222d991fbea7ae
Chutiendat26092003 <chutiendat148@gmail.com>
2022-06-28 22:38:30 +0700

    array

---
+++ array/array-BookStore/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BookStore
+{
+    class Authors
+    {
+        static void Main(string[] args)
+        {
+            Array objAuthorID = Array.CreateInstance(typeof(int), 3);
+            Array objAuthorNames = Array.CreateInstance(typeof(string), 3);
+            Array objAddress = Array.CreateInstance(typeof(string), 3);
+            Array objState = Array.CreateInstance(typeof(string), 3);
+
+
+            string input = "";
+            for (int i = 0; i < objAuthorID.Length; i++)
+            {
+                Console.Write("Enter the ID of author: ");
+                input = Console.ReadLine();
+                objAuthorID.SetValue(Convert.ToInt32(input), i);
+
+                Console.Write("Enter the name of author: ");
+                input = Console.ReadLine();
+                objAuthorNames.SetValue(input, i);
+
+                Console.Write("Enter the address: ");
+                input = Console.ReadLine();
+                objAddress.SetValue(input, i);
+
+                Console.Write("Enter the name of state: ");
+                input = Console.ReadLine();
+                objState.SetValue(input, i);
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n Details of the authors: \n");
+            Console.WriteLine("Author ID\tName\t\tAddress\t\t\tState");
+            for (int i = 0; i < objAuthorID.Length; i++)
+            {
+                Console.Write(objAuthorID.GetValue(i) + "\t\t");
+                Console.Write(objAuthorNames.GetValue(i) + "\t");
+                Console.Write(objAddress.GetValue(i) + "\t\t\t");
+                Console.WriteLine(objState.GetValue(i));
+            }
+
+            Array.Sort(objAuthorNames);
+            Console.WriteLine("\nAuthor names after sorting: \n");
+            for (int i = 0; i <= objAuthorID.GetUpperBound(0); i++)
+            {
+                Console.WriteLine(objAuthorNames.GetValue(i) + "\t");
+            }
+        }
+    }
+}
---
+++ array/array/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+static void Main(string[] args)
+{
+    List<Author> authorList = new List<Author>();
+    int id;
+    string name, address, state;
+
+    while (authorList.Count <= 3)
+    {
+        Console.Write("Enter the ID of author: "); id = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the name of author: "); name = Console.ReadLine();
+        Console.Write("Enter the address: "); address = Console.ReadLine();
+        Console.Write("Enter the name of state: "); state = Console.ReadLine();
+        authorList.Add(new Author(id, name, address, state));
+    }
+
+    Console.WriteLine();
+    foreach (Author author in authorList)
+    {
+        Console.WriteLine(author);
+    }
+
+    //static void display()
+    //{
+    //    Console.Write(""); 
+    //}
+}
+
+class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string State { get; set; }
+        
+    public Author(int id, string name, string address, string state)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Address = address;
+        this.State = state;
+    }
+}
+
+
